e6181e5f79fd9bf4c19235ccae284b4de1f54a4b
Javi Mata <javimata@MacBook-Air-de-JAM.local>
2020-02-03 12:13:14 -0600

    update sensor de humedad

--- sensor_humedad/sensor_humedad.ino
+++ sensor_humedad/sensor_humedad.ino
@@ -1,49 +1,25 @@
 #include <DHT.h>
-#include <DHT_U.h>
 
-// Uncomment whatever type you're using!
-#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+int SENSOR = 2;
+int temp, humedad;
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+DHT dht (SENSOR, DHT11);
 
-const int DHTPin = 2; // what digital pin we're connected to
-
-DHT dht(DHTPin, DHTTYPE);
-
-void setup()
-{
+void setup(){
     Serial.begin(9600);
-    Serial.println("DHTxx test!");
-
     dht.begin();
 }
 
-void loop()
-{
-    // Wait a few seconds between measurements.
-    delay(2000);
+void loop(){
+    humedad = dht.readHumidity();
+    temp    = dht.readTemperature();
 
-    // Reading temperature or humidity takes about 250 milliseconds!
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
+    Serial.print("Temperatura: ");
+    Serial.print(temp);
+    Serial.print("ºC Humedad: ");
+    Serial.print(humedad);
+    Serial.println("%");
 
-    if (isnan(h) || isnan(t))
-    {
-        Serial.println("Failed to read from DHT sensor!");
-        return;
-    }
+    delay(500);
 
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.print(" *C ");
 }
